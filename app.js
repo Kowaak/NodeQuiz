b8e5b6a33bc279dace69922ff573216d14d0788c
Kowaak <zalecki.filip@gmail.com>
2024-05-02 15:03:29 +0200

    wtf

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function(req,res){
-    res.writeHead(200,{'content-type':'text/html'});
-    res.writeHead(200,{'content-type':'application/json'})
-    if (req.url === '/'){
+    //res.writeHead(200,{'content-type':'text/html'});
+    //res.writeHead(200,{'content-type':'application/json'})
+   
         var html = fs.readFileSync('./plik.html','utf8');
         var Header = 'Home page';
         html = html.replace('{ Header }',Header);
-        res.end(html);
-        res.writeHead(200);
-    }else if(req.url === '/product'){
-        res.writeHead(200);
-        res.end('Products');
-    }else if(req.url === '/blog'){
-        res.writeHead(200);
-        res.end('Blog');
-    }else{
-        res.writeHead(404);
-        res.end('404: Not found');
-    }
+        //res.end(html);
+        //res.writeHead(200);
+    
     
     fs.createReadStream('./plik.html').pipe(res);
-    res.writeHead(200);
+    //res.writeHead(200);
     res.end(html);
     
 }).listen(3000);
+
+// URL
+// http://localhost:3000
