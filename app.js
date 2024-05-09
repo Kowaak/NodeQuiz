f98e5e39677c1d4e142bbb8b76f8b9e30a5c2beb
Kowaak <102755037+Kowaak@users.noreply.github.com>
2024-05-09 18:45:25 +0000

    Dodanie CSS i modulu SQL

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const http = require('http');
+const mysql = require('mysql');
 const fs = require('fs');
+/*
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'your_database_user',
+  password: 'your_database_password',
+  database: 'your_database_name'
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to the database!');
 
+  const sql = 'SELECT * FROM your_table_name';
+  connection.query(sql, (err, results) => {
+    if (err) throw err;
+    console.log('Results:', results);
+    connection.end();
+  });
+});
+
+connection.query(sql, (err, results) => {
+  if (err) throw err;
+  console.log('ID:', results[0].id);
+  console.log('Name:', results[0].name);
+  console.log('Email:', results[0].email);
+  connection.end();
+});
+*/
 http.createServer((req, res) => {
   const filePath = './plik.html';
-  const header = 'Home page';
+  const p1 = 'Pytanie 1'; //Do pytan w bazie dodac <br> itp. i modlic sie ze zadziala
+  const p2 = 'Home page'; // i odpowiedzi w tym samym polu
+  const p3 = 'Home page';
 
   fs.readFile(filePath, 'utf8', (err, html) => {
     if (err) {
@@ -12,7 +41,9 @@ http.createServer((req, res) => {
       return;
     }
 
-    html = html.replace('{ Header }', header);
+    html = html.replace('{ p1 }', p1);
+    html = html.replace('{ p2 }', p2);
+    html = html.replace('{ p3 }', p3);
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(html);
